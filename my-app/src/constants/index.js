4ebf9f490e8473daf9ff71c41ca192eb4b758302
HAFKHASNH <khanhhqph20927@fpt.edu.vn>
2023-08-09 13:43:08 +0700

    aaaaaaaaa

--- my-app/src/constants/index.js
+++ my-app/src/constants/index.js
@@ -80,4 +80,98 @@ export const APP_ROUTERS = {
       VALUE: "admin/loaidichvu/delete",
     },
   },
+  NHANVIEN: {
+    INDEX: {
+      LABEL: "QUAN LI Nhan Vien",
+      VALUE: "nhanvien/index",
+    },
+    ADD: {
+      LABEL: "QUAN LI Nhan Vien",
+      VALUE: "nhanvien/create",
+    },
+    UPDATE: {
+      LABEL: "QUAN LI Nhan Vien",
+      VALUE: "nhanvien/update/:id",
+    },
+  },
+  CHUCVU: {
+    INDEX: {
+      LABEL: "QUAN LI CHUC VU",
+      VALUE: "chucvu/index",
+    },
+    ADD: {
+      LABEL: "QUAN LI CHUC VU",
+      VALUE: "chucvu/add",
+    },
+  },
+  NhaCC: {
+    INDEX: {
+      LABEL: "QUAN LI NHACC",
+      VALUE: "nhacc/index",
+    },
+    ADD: {
+      LABEL: "QUAN LI NHACC",
+      VALUE: "nhacc/add",
+    },
+  },
+  PHUKIEN: {
+    INDEX: {
+      LABEL: "QUAN LI PHU KIEN",
+      VALUE: "phukien/index",
+    },
+    ADD: {
+      LABEL: "QUAN LI PHU KIEN",
+      VALUE: "phukien/create",
+    },
+    UPDATE: {
+      LABEL: "QUAN LI PHU KIEN",
+      VALUE: "phukien/update/:id",
+    },
+  },
+  MAUSAC: {
+    INDEX: {
+      LABEL: "QUAN LI MAU SAC",
+      VALUE: "mausac/index",
+    },
+    ADD: {
+      LABEL: "QUAN LI MAU SAC",
+      VALUE: "mausac/create",
+    },
+  },
+  HANGXE: {
+    INDEX: {
+      LABEL: "QUAN LI HANG XE",
+      VALUE: "hangxe/index",
+    },
+    ADD: {
+      LABEL: "QUAN LI HANG XE",
+      VALUE: "hangxe/create",
+    },
+  },
+  MAUXE: {
+    INDEX: {
+      LABEL: "QUAN LI MAU XE",
+      VALUE: "mauxe/index",
+    },
+    ADD: {
+      LABEL: "QUAN LI MAU XE",
+      VALUE: "mauxe/create",
+    },
+  },
+  CHITIETPT: {
+    INDEX: {
+      LABEL: "QUAN LI CHITIETPT",
+      VALUE: "chitietpt/index",
+    },
+    ADD: {
+      LABEL: "QUAN LI CHITIETPT",
+      VALUE: "chitietpt/create",
+    },
+  },
+  XEMLICH: {
+    INDEX: {
+      LABEL: "QUAN LI XEMLICH",
+      VALUE: "xemlich/index",
+    },
+  },
 };
