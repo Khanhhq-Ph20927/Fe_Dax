4de23058c38f2ab1259a14b87cef642658f2a64c
DESKTOP-KH4NTSB\admin <longthph20602@fpt.edu.vn>
2023-08-09 13:48:42 +0700

    long

--- my-app/src/constants/index.js
+++ my-app/src/constants/index.js
@@ -1,99 +1,177 @@
 export const APP_ROUTERS = {
-    DASHBOAR: 'dashboard',
+    DASHBOAR: "dashboard",
+    DichVu: {
+      INDEX: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/dichvu/index",
+      },
+      PAGE: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/dichvu/index/page/:number",
+      },
+      SEARCH_TEN: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/dichvu/index/search_ten/:ten",
+      },
+      SEARCH_TEN2: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/dichvu/index/search_ten/:ten/:number",
+      },
+      SEARCH_LOAIDV: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/dichvu/index/search_loai/:loaiDV",
+      },
+      SEARCH_LOAIDV2: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/dichvu/index/search_loai/:loaiDV/:number",
+      },
+      SORT: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/dichvu/index/sort/:field",
+      },
+      ADD: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/dichvu/add",
+      },
+      DETAIL: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/dichvu/detail/:id",
+      },
+      UPDATE: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/dichvu/update/:id",
+      },
+      DELETE: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/dichvu/delete",
+      },
+    },
+    LoaiDichVu: {
+      INDEX: {
+        LABEL: "QUAN LI LOAI DICH VU",
+        VALUE: "admin/loaidichvu/index",
+      },
+      PAGE: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/loaidichvu/index/page/:number",
+      },
+      search: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/loaidichvu/index/search/:ten",
+      },
+      SORT: {
+        LABEL: "QUAN LI DICH VU",
+        VALUE: "admin/loaidichvu/index/sort/:field",
+      },
+      ADD: {
+        LABEL: "QUAN LI LOAI DICH VU",
+        VALUE: "admin/loaidichvu/add",
+      },
+      DETAIL: {
+        LABEL: "QUAN LI LOAI DICH VU",
+        VALUE: "admin/loaidichvu/detail/:id",
+      },
+      UPDATE: {
+        LABEL: "QUAN LI LOAI DICH VU",
+        VALUE: "admin/loaidichvu/update/:id",
+      },
+      DELETE: {
+        LABEL: "QUAN LI LOAI DICH VU",
+        VALUE: "admin/loaidichvu/delete",
+      },
+    },
     NHANVIEN: {
-        INDEX: {
-            LABEL: 'QUAN LI Nhan Vien',
-            VALUE: 'nhanvien/index'
-        },
-        ADD: {
-            LABEL: 'QUAN LI Nhan Vien',
-            VALUE: 'nhanvien/create'
-        },
-    UPDATE: {
-            LABEL: 'QUAN LI Nhan Vien',
-            VALUE: 'nhanvien/update/:id'
-        }
+      INDEX: {
+        LABEL: "QUAN LI Nhan Vien",
+        VALUE: "nhanvien/index",
+      },
+      ADD: {
+        LABEL: "QUAN LI Nhan Vien",
+        VALUE: "nhanvien/create",
+      },
+      UPDATE: {
+        LABEL: "QUAN LI Nhan Vien",
+        VALUE: "nhanvien/update/:id",
+      },
     },
     CHUCVU: {
-        INDEX: {
-            LABEL: 'QUAN LI CHUC VU',
-            VALUE: 'chucvu/index'
-        },
-        ADD: {
-            LABEL: 'QUAN LI CHUC VU',
-            VALUE: 'chucvu/add'
-        }
+      INDEX: {
+        LABEL: "QUAN LI CHUC VU",
+        VALUE: "chucvu/index",
+      },
+      ADD: {
+        LABEL: "QUAN LI CHUC VU",
+        VALUE: "chucvu/add",
+      },
     },
     NhaCC: {
-        INDEX: {
-            LABEL: 'QUAN LI NHACC',
-            VALUE: 'nhacc/index'
-        },
-        ADD: {
-            LABEL: 'QUAN LI NHACC',
-            VALUE: 'nhacc/add'
-        }
+      INDEX: {
+        LABEL: "QUAN LI NHACC",
+        VALUE: "nhacc/index",
+      },
+      ADD: {
+        LABEL: "QUAN LI NHACC",
+        VALUE: "nhacc/add",
+      },
     },
-        PHUKIEN: {
-        INDEX: {
-            LABEL: 'QUAN LI PHU KIEN',
-            VALUE: 'phukien/index'
-        },
-        ADD: {
-            LABEL: 'QUAN LI PHU KIEN',
-            VALUE: 'phukien/create'
-        },
-        UPDATE: {
-            LABEL: 'QUAN LI PHU KIEN',
-            VALUE: 'phukien/update/:id'
-        }
+    PHUKIEN: {
+      INDEX: {
+        LABEL: "QUAN LI PHU KIEN",
+        VALUE: "phukien/index",
+      },
+      ADD: {
+        LABEL: "QUAN LI PHU KIEN",
+        VALUE: "phukien/create",
+      },
+      UPDATE: {
+        LABEL: "QUAN LI PHU KIEN",
+        VALUE: "phukien/update/:id",
+      },
     },
     MAUSAC: {
-        INDEX: {
-            LABEL: 'QUAN LI MAU SAC',
-            VALUE: 'mausac/index'
-        },
-        ADD: {
-            LABEL: 'QUAN LI MAU SAC',
-            VALUE: 'mausac/create'
-        }
+      INDEX: {
+        LABEL: "QUAN LI MAU SAC",
+        VALUE: "mausac/index",
+      },
+      ADD: {
+  LABEL: "QUAN LI MAU SAC",
+        VALUE: "mausac/create",
+      },
     },
     HANGXE: {
-        INDEX: {
-            LABEL: 'QUAN LI HANG XE',
-            VALUE: 'hangxe/index'
-        },
-        ADD: {
-            LABEL: 'QUAN LI HANG XE',
-            VALUE: 'hangxe/create'
-        }
+      INDEX: {
+        LABEL: "QUAN LI HANG XE",
+        VALUE: "hangxe/index",
+      },
+      ADD: {
+        LABEL: "QUAN LI HANG XE",
+        VALUE: "hangxe/create",
+      },
     },
     MAUXE: {
-        INDEX: {
-            LABEL: 'QUAN LI MAU XE',
-            VALUE: 'mauxe/index'
-        },
-        ADD: {
-            LABEL: 'QUAN LI MAU XE',
-            VALUE: 'mauxe/create'
-        }
+      INDEX: {
+        LABEL: "QUAN LI MAU XE",
+        VALUE: "mauxe/index",
+      },
+      ADD: {
+        LABEL: "QUAN LI MAU XE",
+        VALUE: "mauxe/create",
+      },
     },
     CHITIETPT: {
-        INDEX: {
-            LABEL: 'QUAN LI CHITIETPT',
-            VALUE: 'chitietpt/index'
-        },
-        ADD: {
-            LABEL: 'QUAN LI CHITIETPT',
-            VALUE: 'chitietpt/create'
-        }
+      INDEX: {
+        LABEL: "QUAN LI CHITIETPT",
+        VALUE: "chitietpt/index",
+      },
+      ADD: {
+        LABEL: "QUAN LI CHITIETPT",
+        VALUE: "chitietpt/create",
+      },
     },
     XEMLICH: {
-        INDEX: {
-            LABEL: 'QUAN LI XEMLICH',
-            VALUE: 'xemlich/index'
-        },
-       
-    },
-
-}
+      INDEX: {
+        LABEL: "QUAN LI XEMLICH",
+        VALUE: "xemlich/index",
+      }
+    }
+  };
